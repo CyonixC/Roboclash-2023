932d5e62927a5dced9c69a4dc0f033e95cb865a7
CyonixC <35833065+CyonixC@users.noreply.github.com>
2023-01-10 15:32:47 +0800

    change main halfway

--- main.ino
+++ main.ino
@@ -16,56 +16,107 @@ const int R_ENPIN = 6;
 // Servo pins
 // REMEMBER TO CHANGE AFTER WIRING
 const int HOOK_PIN = 0;
-const int ARM_PIN = 0;
-const int CLAW_PIN = 0;
+const int ARM_PIN = 8;
+const int CLAW_PIN = 7;
+const int TEST_SERVO = 0;
 
 // Servos
 Servo HookServo;
 Servo ClawArmServo;
 Servo ClawServo;
+Servo TestServo;
+
+const int DEFAULT_MAX_SPEED = 3;
 
 //Controller settings
-byte interruptPin = 3;
-byte channelAmount = 10;
+byte interruptPin = 9;
+byte channelAmount = 8;
 PPMReader ppm(interruptPin, channelAmount);
 
+// Servo structs
+struct ServoStruct {
+  Servo servo;
+  int angle;
+  int maxSpeed;
+};
+
+// struct ServoController {
+//   ServoStruct servo1;
+//   ServoStruct servo2;
+//   ServoStruct servo3;
+//   int targetAngle1;
+//   int targetAngle2;
+//   int targetAngle3;
+// };
+
 void setup(){
-    // Attach servo pins
-    HookServo.attach(HOOK_PIN);
-    ClawArmServo.attach(ARM_PIN);
-    ClawServo.attach(CLAW_PIN);
+  // Attach servo pins
+  HookServo.attach(HOOK_PIN);
+  ClawArmServo.attach(ARM_PIN);
+  ClawServo.attach(CLAW_PIN);
+  // TestServo.attach(TEST_SERVO);
 
-    // Set L298N motor pins to OUTPUT
-    pinMode(L_FWDPIN, OUTPUT);
-    pinMode(L_BWDPIN, OUTPUT);
-    pinMode(L_ENPIN, OUTPUT);
+  // Reset all motors to 0 position
+  HookServo.write(0);
+  ClawArmServo.write(0);
+  ClawServo.write(0);
 
-    pinMode(R_FWDPIN, OUTPUT);
-    pinMode(R_BWDPIN, OUTPUT);
-    pinMode(R_ENPIN, OUTPUT);
+  // ServoController servoController = {hookStruct, armStruct, clawStruct, 0, 0, 0};
 
-    // Start serial comms
-    Serial.begin(BAUD_RATE);
+  // Set L298N motor pins to OUTPUT
+  pinMode(L_FWDPIN, OUTPUT);
+  pinMode(L_BWDPIN, OUTPUT);
+  pinMode(L_ENPIN, OUTPUT);
+
+  pinMode(R_FWDPIN, OUTPUT);
+  pinMode(R_BWDPIN, OUTPUT);
+  pinMode(R_ENPIN, OUTPUT);
+  pinMode(2, INPUT);
+
+  // Start serial comms
+  Serial.begin(BAUD_RATE);
 }
+
+ServoStruct hookStruct = {HookServo, 0, DEFAULT_MAX_SPEED};
+ServoStruct armStruct = {ClawArmServo, 0, DEFAULT_MAX_SPEED};
+ServoStruct clawStruct = {ClawServo, 0, DEFAULT_MAX_SPEED};
 
 void loop(){
-    // Controller channels, comment out the ones we don't need!
-    // int rightHorz = readChannel(1, false);
-    // int leftVert = readChannel(2, false);
-    // int rightVert = readChannel(3, false);
-    // int leftHorz = readChannel(4, false);
-    // int leftBumper = readChannel(6, false);
-    // int rightBumper = readChannel(7, false);
-    // int left3Switch = readChannel(8, false);
-    // int right2Switch = readChannel(9, false);
-    // int right3Switch = readChannel(10, false);
+  // Controller channels, comment out the ones we don't need!
+  // int rightHorz = readChannel(1, false);
+  // int leftVert = readChannel(2, false);
+  // int rightVert = readChannel(3, false);
+  // int leftHorz = readChannel(4, false);
+  // int leftBumper = readChannel(6, false);
+  // int rightBumper = readChannel(7, false);
+  // int left3Switch = readChannel(8, false);
+  // int right2Switch = readChannel(9, false);
+  int right3Switch = readChannel(10, false);
 
-    debug();
+  // long hookControl = map(leftBumper, 1000, 2000, -1, 1);
+  // long clawControl = map(rightBumper, 1000, 2000, -1, 1);
+  long armControl = map(right3Switch, 1000, 2000, -1, 1);
+  armControl = round(armControl);
+  // long speedControl = map(rightVert, 1000, 2000, 0, 1);
+  // long directionControl = map(leftVert, 1000, 2000, -1, 1);
+  // long turningControl = map(leftHorz, 1000, 2000, -1, 1);
 
-    
+  incrementServoAngle(armStruct, armControl, 180, 0);
+  delay(15);
 }
 
-void debug() {
+
+
+
+
+// ----------------------
+// Custom Functions below
+// ----------------------
+
+
+
+
+void debugChannels() {
     int rightHorz = readChannel(1, true);
     int leftVert = readChannel(2, true);
     int rightVert = readChannel(3, true);
@@ -75,7 +126,40 @@ void debug() {
     int left3Switch = readChannel(8, true);
     int right2Switch = readChannel(9, true);
     Serial.println("");
-    delay(1000);
+    delay(3000);
+}
+
+void testServo(Servo servo){
+  servo.write(0); // move MG996R's shaft to angle 0°
+  delay(1000); // wait for one second
+  servo.write(45); // move MG996R's shaft to angle 45°
+  delay(1000); // wait for one second 
+  servo.write(90); // move MG996R's shaft to angle 90°
+  delay(1000); // wait for one second
+  servo.write(120); // move MG996R's shaft to angle 120°
+  delay(1000); // wait for one second
+}
+
+void setServoAngle(ServoStruct servoStruct, int targetAngle) {
+  // Incomplete!!!
+  int speedLimit = servoStruct.maxSpeed;
+  int currentAngle = servoStruct.angle;
+  Servo servo = servoStruct.servo;
+  for (int angle = currentAngle; angle <= targetAngle; angle += speedLimit) {
+    servo.write(angle);
+    delay(15);
+  }
+}
+
+// void remapControls(int input, int )
+
+void incrementServoAngle(ServoStruct servoStruct, int angleDifference, int maxAngle, int minAngle) {
+  int speedLimit = servoStruct.maxSpeed;
+  int currentAngle = servoStruct.angle;
+  int targetAngle = currentAngle + angleDifference;
+  if (targetAngle > maxAngle) {targetAngle = maxAngle;}
+  if (targetAngle < minAngle) {targetAngle = minAngle;}
+  servoStruct.servo.write(targetAngle);
 }
 
 void move(bool dir, int throttleVal, int steeringVal, int leftLimit, int rightLimit, int L_EN, int R_EN) {
